c2e9efb547eca47b655b0d833200ee2d9c2a3aef
Joseph <josephvincentlimbaroc@gmail.com>
2025-07-02 11:10:00 +0800

    added dark and light mode

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="bg-gray-100 min-h-screen dark:bg-slate-300">
-    <TransitionRoot as="template" :show="sidebarOpen">
-      <Dialog class="relative z-50 lg:hidden" @close="sidebarOpen = false">
+  <div class="bg-gray-100 dark:bg-slate-600 h-screen">
+    <TransitionRoot as="template" :show="state.open">
+      <Dialog class="relative z-50 lg:hidden" @close="state.open = false">
         <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
           enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100"
           leave-to="opacity-0">
-          <div class="fixed inset-0 bg-gray-900/80" />
+          <div class="fixed inset-0 bg-gray-900/50" />
         </TransitionChild>
 
         <div class="fixed inset-0 flex">
@@ -17,29 +17,27 @@
               <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
                 enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
                 <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
-                  <button type="button" class="-m-2.5 p-2.5" @click="sidebarOpen = false">
+                  <button type="button" class="-m-2.5 p-2.5" @click="state.open = false">
                     <span class="sr-only">Close sidebar</span>
                     <XMarkIcon class="size-6 text-white" aria-hidden="true" />
                   </button>
                 </div>
               </TransitionChild>
-
-              <!-- Sidebar component, swap this element with another sidebar if you like -->
-              <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-blue-600 px-6 pb-4 dark:bg-slate-800">
-                <div class="flex h-16 shrink-0 items-center">
-                  <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=white"
-                    alt="Your Company" />
+              <div
+                class="flex grow flex-col gap-y-5 overflow-y-auto bg-blue-500 dark:bg-slate-800 px-6 pb-4 ring-1 ring-white/10">
+                <div class="flex h-16 shrink-0 items-center justify-center">
+                  <span class="font-bold text-white text-center">
+                    Sports Athlete Management System
+                  </span>
                 </div>
                 <nav class="flex flex-1 flex-col">
                   <ul role="list" class="flex flex-1 flex-col gap-y-7">
                     <li>
                       <ul role="list" class="-mx-2 space-y-1">
                         <li v-for="item in navigation" :key="item.name">
-                          <NuxtLink :to="item.href"
-                            :class="[item.activeRouteNames.includes($route.name) ? 'bg-blue-700 text-white dark:bg-slate-700' : 'text-blue-200 hover:bg-blue-700 dark:hover:bg-slate-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-xl font-semibold']">
-                            <component :is="item.icon"
-                              :class="[item.activeRouteNames.includes($route.name) ? 'text-white' : 'text-blue-200 dark:text-slate-200 group-hover:text-white', 'size-6 shrink-0']"
-                              aria-hidden="true" />
+                          <NuxtLink @click="state.open = false" :to="item.href"
+                            :class="[item.activeRouteNames.includes($route.name) ? 'bg-blue-800 dark:bg-slate-700 text-white' : 'text-white hover:bg-blue-800 dark:hover:bg-slate-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-base font-semibold']">
+                            <component :is="item.icon" class="size-6 shrink-0" aria-hidden="true" />
                             {{ item.name }}
                           </NuxtLink>
                         </li>
@@ -54,13 +52,12 @@
       </Dialog>
     </TransitionRoot>
 
-    <!-- Static sidebar for desktop -->
     <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-blue-600 px-6 pb-4 dark:bg-slate-800">
-        <div class="flex h-16 shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=white"
-            alt="Your Company" />
+      <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-blue-500 dark:bg-slate-800 px-6 pb-4">
+        <div class="flex h-16 shrink-0 items-center justify-center">
+          <span class="font-bold text-white text-center text-center">
+            Sports Athlete Management System
+          </span>
         </div>
         <nav class="flex flex-1 flex-col">
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
@@ -68,10 +65,8 @@
               <ul role="list" class="-mx-2 space-y-1">
                 <li v-for="item in navigation" :key="item.name">
                   <NuxtLink :to="item.href"
-                    :class="[item.activeRouteNames.includes($route.name) ? 'bg-blue-700 dark:bg-slate-700 text-white' : 'text-blue-200 dark:text-slate-200 hover:bg-blue-700 dark:hover:bg-slate-700 hover:text-white', 'group flex gap-x-3 rounded-md p-3 text-lg font-semibold']">
-                    <component :is="item.icon"
-                      :class="[item.activeRouteNames.includes($route.name) ? 'text-white' : 'text-blue-200 dark:text-slate-200 group-hover:text-white', 'size-6 shrink-0']"
-                      aria-hidden="true" />
+                    :class="[item.activeRouteNames.includes($route.name) ? 'bg-blue-800 dark:bg-slate-700 text-white' : 'text-white hover:bg-blue-800 dark:hover:bg-slate-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-base font-semibold']">
+                    <component :is="item.icon" class="size-6 shrink-0" aria-hidden="true" />
                     {{ item.name }}
                   </NuxtLink>
                 </li>
@@ -84,40 +79,38 @@
 
     <div class="lg:pl-72">
       <div
-        class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-        <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" @click="sidebarOpen = true">
+        class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 dark:border-gray-600 bg-white dark:bg-slate-800 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+        <button type="button" class="-m-2.5 p-2.5 text-black lg:hidden" @click="state.open = true">
           <span class="sr-only">Open sidebar</span>
-          <Bars3Icon class="size-6" aria-hidden="true" />
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
         </button>
 
-        <!-- Separator -->
-        <div class="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
+        <div class="h-6 w-px bg-gray-200 dark:bg-white lg:hidden" aria-hidden="true" />
 
-        <div class="flex flex-1 justify-end gap-x-4 self-stretch lg:gap-x-6">
+        <div class="flex flex-1 gap-x-4 justify-end self-stretch lg:gap-x-6">
           <div class="flex items-center gap-x-4 lg:gap-x-6">
+
             <button type="button" @click="toggleTheme" class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
               <MoonIcon class="size-6" aria-hidden="true" v-if="state.color == 'light'" />
-              <SunIcon class="size-6" aria-hidden="true" v-else />
-            </button>
-            <button type="button" class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="size-6" aria-hidden="true" />
+              <SunIcon class="size-6 text-yellow-600" aria-hidden="true" v-if="state.color == 'dark'" />
             </button>
 
-            <!-- Separator -->
-            <div class="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10" aria-hidden="true" />
+            <div class="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10 dark:bg-white" aria-hidden="true" />
 
-            <!-- Profile dropdown -->
             <Menu as="div" class="relative">
-              <MenuButton class="relative flex items-center">
-                <span class="absolute -inset-1.5" />
+              <MenuButton class="-m-1.5 flex items-center p-1.5 cursor-pointer">
                 <span class="sr-only">Open user menu</span>
-                <img class="size-8 rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt="" />
-                <span class="hidden lg:flex lg:items-center">
-                  <span class="ml-4 text-sm/6 font-semibold text-gray-900" aria-hidden="true">Tom Cook</span>
-                  <ChevronDownIcon class="ml-2 size-5 text-gray-400" aria-hidden="true" />
+                <span
+                  class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-blue-500 dark:bg-slate-900">
+                  <span class="text-base font-bold leading-none text-white">
+                    {{ state.user?.firstname.charAt(0) }}{{ state.user?.lastname.charAt(0) }}
+                  </span>
+                </span>
+                <span class="flex items-center">
+                  <span class="ml-4 text-base font-semibold leading-6 text-black dark:text-white" aria-hidden="true">
+                    {{ state.user?.firstname }} {{ state.user?.lastname }}
+                  </span>
+                  <ChevronDownIcon class="ml-2 h-5 w-5 text-black" aria-hidden="true" />
                 </span>
               </MenuButton>
               <transition enter-active-class="transition ease-out duration-100"
@@ -126,10 +119,11 @@
                 leave-to-class="transform opacity-0 scale-95">
                 <MenuItems
                   class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                  <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                  <a :href="item.href"
-                    :class="[active ? 'bg-gray-50 outline-none' : '', 'block px-3 py-1 text-sm/6 text-gray-900']">{{
-                      item.name }}</a>
+                  <MenuItem>
+                  <a @click="logoutUser"
+                    class='block px-3 py-1 text-base leading-6 text-black hover:bg-gray-50 cursor-pointer'>
+                    Sign out
+                  </a>
                   </MenuItem>
                 </MenuItems>
               </transition>
@@ -148,7 +142,6 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import {
   Dialog,
   DialogPanel,
@@ -161,46 +154,35 @@ import {
 } from '@headlessui/vue'
 import {
   Bars3Icon,
-  BellIcon,
-  Cog6ToothIcon,
-  UserIcon,
   RectangleGroupIcon,
-  UserGroupIcon,
   XMarkIcon,
   MoonIcon,
-  SunIcon
+  SunIcon,
 } from '@heroicons/vue/24/outline'
-import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+import { ChevronDownIcon, UserGroupIcon } from '@heroicons/vue/20/solid'
 
-type Theme = 'light' | 'dark'
-
-const setColorTheme = (newTheme: Theme) => {
-  useColorMode().preference = newTheme
-}
+const colorMode = useColorMode()
 
 const state = reactive({
-  color: 'light'
+  color: colorMode.preference,
+  open: false
 })
 
-function toggleTheme(){
-  if(state.color == 'light'){
-    state.color = 'dark'
-    setColorTheme(state.color);
-  }else{
-    state.color = 'light'
-    setColorTheme(state.color);
-  }
+onMounted(() => {
+  state.color = colorMode.preference
+})
+
+watch(() => colorMode.preference, (newVal) => {
+  state.color = newVal
+})
+
+function toggleTheme() {
+  const newTheme = colorMode.preference === 'light' ? 'dark' : 'light'
+  colorMode.preference = newTheme
 }
 
 const navigation = [
   { name: 'Dashboard', href: '/dashboard', icon: RectangleGroupIcon, activeRouteNames: ['dashboard'] },
   { name: 'Athletes', href: '/athletes', icon: UserGroupIcon, activeRouteNames: ['athletes'] },
-  { name: 'Coaches', href: '/coaches', icon: UserIcon, activeRouteNames: ['coaches'] },
 ]
-const userNavigation = [
-  { name: 'Your profile', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
-
-const sidebarOpen = ref(false)
 </script>
--- pages/athletes/index.vue
+++ pages/athletes/index.vue
@@ -4,8 +4,8 @@
         <Title>{{ runtimeConfig?.public?.appName }} | Athletes</Title>
     </Head>
 
-    <div>
-        <p class="dark:text-white">Athletes</p>
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <TableAthlete :head=state.head :body="state.body" />
     </div>
 </template>
 
@@ -15,6 +15,25 @@ const runtimeConfig = useRuntimeConfig()
 
 definePageMeta({
     layout: 'main'
+})
+
+const state = reactive({
+    head: [
+        { name: 'Full name' },
+        { name: 'Gender' },
+        { name: 'Date of birth' },
+        { name: 'Age' },
+        { name: 'Contact no.' },
+    ],
+    body: {
+        data: [
+            { id: 1, full_name: 'Kim Tan', gender: 'Female', birthdate: 'August 18, 1994', age: 31, phone_number: '09987654321' },
+            { id: 1, full_name: 'Kim Tan', gender: 'Female', birthdate: 'August 18, 1994', age: 31, phone_number: '09987654321' },
+            { id: 1, full_name: 'Kim Tan', gender: 'Female', birthdate: 'August 18, 1994', age: 31, phone_number: '09987654321' },
+            { id: 1, full_name: 'Kim Tan', gender: 'Female', birthdate: 'August 18, 1994', age: 31, phone_number: '09987654321' },
+            { id: 1, full_name: 'Kim Tan', gender: 'Female', birthdate: 'August 18, 1994', age: 31, phone_number: '09987654321' },
+        ]
+    }
 })
 
 </script>
