6d692c34314902e1fc639390ad47c83578207b42
Joseph <josephvincentlimbaroc@gmail.com>
2025-07-09 15:09:54 +0800

    fix minor spaces

--- pages/athletes/[uuid]/careers/[uuid]/index.vue
+++ pages/athletes/[uuid]/careers/[uuid]/index.vue
@@ -10,18 +10,18 @@
         <FormBackButton @click="goToPreviousPage" />
         <div class="mt-8">
             <div class="overflow-hidden rounded-lg bg-white shadow mb-6">
-                <div class="px-4 py-5 sm:p-6 flex items-center gap-x-4">
-                    <div>
+                <div class="px-4 py-5 sm:p-6 grid grid-cols-1 sm:flex sm:items-center sm:gap-x-4">
+                    <div class="flex items-center justify-center">
                         <img v-if="state.career?.model?.photo"
                             class="size-24 rounded-full ring-4 ring-white sm:size-32 bg-white"
                             :src="state.career?.model?.photo" alt="profile photo" />
                         <img v-else class="size-24 rounded-full ring-4 ring-white sm:size-32 bg-white" :src="avatarUrl"
                             alt="profile photo" />
                     </div>
-                    <div>
+                    <div class="flex items-center justify-center mt-4 sm:mt-0">
                         <span class="text-4xl font-bold">
                             {{ state.career?.model?.firstname }}
-                            {{ state.career?.model?.middlename ?? '' }}
+                            {{ (state.career?.model?.middlename != 'null') ? state.career?.model?.middlename : '' }}
                             {{ state.career?.model?.lastname }}
                         </span>
                     </div>
