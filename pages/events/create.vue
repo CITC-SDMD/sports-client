4589e0e1075409bb494626de3bf4065131b2b58b
unknown <jfdiems@acdeducation.com>
2025-09-26 22:54:34 +0800

    Added sweetlart for progress bar

--- pages/events/create.vue
+++ pages/events/create.vue
@@ -22,14 +22,13 @@
 <script setup lang="ts">
 import { eventService } from '@/api/event/EventService'
 import { useAlert } from '@/composables/alert'
-import { useSweetAlert } from '@/composables/sweetAlert'
+import Swal from 'sweetalert2'
 import moment from 'moment';
-
 
 const runtimeConfig = useRuntimeConfig()
 
 const { successAlert } = useAlert()
-const { successSweetAlert } = useSweetAlert()
+
 
 const route = useRoute()
 const path = route.fullPath
@@ -46,12 +45,15 @@ definePageMeta({
 })
 
 const state = reactive({
+    body: [] as any,
+    event_uuid: null as any,
+    searchFilter: null as any,
+    search: null as any,
     isPageLoading: false,
     error: null as any,
 })
 
 async function saveEvent(data: any) {
-    state.isPageLoading = true
     try {
         let params = {
             event_name: data.event_name,
@@ -86,10 +88,40 @@ async function saveEvent(data: any) {
         }
         const response = await eventService.createEvent(params)
         if (response.data) {
+            state.event_uuid = response.data?.uuid
             successAlert('Success!', 'Event created.')
-            // goToPreviousPage()
-            successSweetAlert('Are you sure?', 'Do you want to generate a list of qualifiers?')
+            const result = await Swal.fire({
+                title: "Are you sure?",
+                text: "Do you want to generate a list of qualifiers?",
+                icon: "info",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Confirm it!"
+            });
 
+            if (result.isConfirmed) {
+                await getQualifiedAthletes()
+            } else {
+                goToPreviousPage()
+                state.isPageLoading = false
+            }
+        }
+    } catch (error) {
+        state.error = error
+    }
+}
+
+async function getQualifiedAthletes() {
+    state.isPageLoading = true
+    try {
+        let params = {
+            search: state.search
+        }
+        const response = await eventService.fetchQualifiedAthletes(params, state.event_uuid)
+        if (response.data) {
+            state.body = response
+            goToPreviousPage()
         }
     } catch (error) {
         state.error = error
